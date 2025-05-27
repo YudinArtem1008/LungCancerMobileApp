6071f485606413497a52419ace1c93872d097091
android <yudinart12008@gmail.com>
2025-05-27 16:42:26 +1000

    I've fixed problems with server and I'VE INSTALLED THIS STUPID PYTORCH THERE

--- demo (1)/demo/src/main/java/com/example/android_server/ServerForAndroidApplication.java
+++ demo (1)/demo/src/main/java/com/example/android_server/ServerForAndroidApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.android_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ServerForAndroidApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ServerForAndroidApplication.class, args);
 	}
 
 }
--- demo (1)/demo/src/test/java/com/example/android_server/ServerForAndroidApplicationTests.java
+++ demo (1)/demo/src/test/java/com/example/android_server/ServerForAndroidApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.android_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ServerForAndroidApplicationTests {
 
 	@Test
 	void contextLoads() {
